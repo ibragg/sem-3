21efd9a3a75135c342db55d5891c2ecebefa3cff
ibragg <69543053+ibragg@users.noreply.github.com>
2020-12-19 04:26:14 +0300

    Create client.c

---
+++ taaask/client.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+#define INIT(n, sem, op, flg) sops[n].sem_num = sem;                                  \
+                                                        sops[n].sem_op = op;                                    \
+                                                        sops[n].sem_flg = flg;
+
+
+#define SEM(sem, op, flg)             INIT(0, sem, op, flg)                                 \
+                                                        semop(semid, sops, 1);
+
+
+#define PAGE_SIZE 4096
+#define ftokname "server.c"
+
+int Sem(int semId, int semNum, int initVal) {
+    return semctl(semId, semNum, SETVAL, initVal);
+}
+
+void File(char *filename, int semid, void *addr) {
+
+    FILE *flin = fopen(filename, "rb");
+
+    struct sembuf sops[4];
+
+    INIT(0, 0, 0, 0)
+    INIT(1, 0, 1, SEM_UNDO)
+    semop(semid, sops, 2);
+
+    Sem(semid, 3, 1);
+
+    INIT(0, 1, -2, 0)
+    INIT(1, 1, 2, 0)
+    INIT(2, 4, 1, SEM_UNDO);
+    semop(semid, sops, 3);
+
+    int len = 4092;
+
+    while (len == 4092) {
+        SEM(2, -1, 0)
+
+        len = fread(addr + 4, 1, 4092, flin);
+        *(int *) addr = len;
+
+        SEM(3, 1, 0)
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int semid = semget(ftok(ftokname, 10), 5, IPC_CREAT | 0666);
+    int shmid = shmget(ftok(ftokname, 10), PAGE_SIZE, IPC_CREAT | 0666);
+    void *addr = shmat(shmid, NULL, 0);
+    File(argv[1], semid, addr);
+    return 0;
+}
